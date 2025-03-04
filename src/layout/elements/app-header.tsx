287d6f222630e5f76b6a3126be9501fdc6a7b468
areebaqamar021 <areebaqamar021@gmail.com>
2025-03-04 22:18:33 +0500

    Remove unused images and update AppHeader styling; add custom beige color to Tailwind config

--- src/layout/elements/app-header.tsx
+++ src/layout/elements/app-header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import { useSignout, useUser } from '@src/apis';
 import { SearchOutlined } from '@ant-design/icons';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 const { Header } = Layout
 
@@ -12,10 +12,11 @@ function AppHeader() {
   const { mutateAsync: signOut } = useSignout();
   const pathname = usePathname();
   return (
-    <Header className='bg-white flex justify-between items-center h-20'>
+    <Header className='bg-customBeige flex justify-between items-center h-20'>
       {user ? (<Input className='w-52' placeholder='Search' prefix={<SearchOutlined />} />) : (
         <Link href="/">
-          <Image src={"/images/logo.png"} alt='' width={100} height={100} />
+          {/* <Image src={"/images/logo.png"} alt='' width={100} height={100} /> */}
+          <h1 className='w-20 text-black font-bold text-3xl text-center'>Medium</h1>
         </Link>
       )}
       {user ? (
@@ -28,6 +29,7 @@ function AppHeader() {
         </Button>
       ) : (
         pathname !== "/auth" && <Link href="/auth">
+          
           <Button
             type="text"
             className="text-xl font-semibold"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        customBeige: '#F7F4ED',
+      },
+
     },
   },
   plugins: [],
