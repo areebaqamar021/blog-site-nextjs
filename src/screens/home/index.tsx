30a089b2012eb80d2f1204836575702611d16d43
areebaqamar021 <areebaqamar021@gmail.com>
2025-03-07 23:01:03 +0500

    Refactor footer component: remove old AppFooter, add new AppFooter in home screen, and introduce Banner component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,11 @@
 "use client"
 import React from 'react'
+import Banner from './elements/banner'
 
 function HomeScreen() {
   return (
     <div>
-      HomeScreen
+       <Banner />
     </div>
   )
 }
