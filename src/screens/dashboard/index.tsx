703d6b5a3a63cf16d2e84182f7dacc10fa21ec81
areebaqamar021 <areebaqamar021@gmail.com>
2025-03-08 01:00:50 +0500

    Refactor DashBoardScreen: implement blog fetching logic, update layout with responsive cards, and enhance styling

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { Card, Col, Row } from 'antd';
+import { useGetBlogs } from '@src/apis';
+import Link from 'next/link';
 
-function DashBoardScreen() {
+const DashBoardScreen = () => {
+  // Fetch only published posts
+  const { data: blogs } = useGetBlogs({
+    user: true,
+    published: true, // Show only published blogs
+  });
+
   return (
-    <div>
-      DashBoardScreen
+    <div className='p-5'>
+      <Row gutter={16}>
+        {blogs?.map(blog => (
+          <Col key={blog.id} xs={24} sm={12} md={8} lg={6}>
+            <Link href={`/${blog.slug}`} className='block'>
+              <Card title={blog.title} variant='borderless' hoverable>
+                <div className='text-gray-600 text-sm'>{blog.description}</div>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default DashBoardScreen
+export default DashBoardScreen;
