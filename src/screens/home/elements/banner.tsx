dc112589003d93344bebacfda4eedb0819adfa26
areebaqamar021 <areebaqamar021@gmail.com>
2025-03-08 00:07:01 +0500

    Refactor AppFooter: replace social media icons with navigation links; update Banner component styling and add AppFooter to HomeScreen

--- src/screens/home/elements/banner.tsx
+++ src/screens/home/elements/banner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Banner() {
   return (
-    <div className="w-full h-screen bg-customBeige flex flex-col justify-center items-center text-center">
+    <div className="w-full h-screen bg-customBeige flex flex-col justify-center items-center text-center border-b border-black">
       <h1 className="text-6xl font-bold text-black leading-tight">Human</h1>
       <h1 className="text-6xl font-bold text-black leading-tight">stories & ideas</h1>
       <p className="text-lg text-black mt-4">A place to read, write, and deepen your understanding</p>
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React from 'react'
 import Banner from './elements/banner'
+import AppFooter from './elements/app-footer'
 
 function HomeScreen() {
   return (
     <div>
        <Banner />
+       <AppFooter/>
     </div>
   )
 }
